dd3a031c1afa27a2dbf4cce138940c03e3b9fbe3
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2022-12-13 19:58:41 +0300

    1.1: Implemented a list using LinearLayout

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun showList(list: List<ShopItem>) {
         linearLayoutShopList.removeAllViews()
         for (shopItem in list) {
