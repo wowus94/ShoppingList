42d38d996e89a01e853ca76c764177e228453938
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-02-05 20:08:17 +0300

    Add database

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopItemDbModel.kt
@@ -0,0 +1,16 @@
+package com.example.shoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "shop_items")
+class ShopItemDbModel(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean
+
+)
